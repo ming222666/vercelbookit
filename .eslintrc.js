c0d90916f8a057f60cb04f2114c1b5922437fe0e
ming222555 <ming222555@yahoo.com>
2022-05-23 20:28:24 +0800

    Install prettier and its eslint related plugins. Replace .eslintrc.json by .eslintrc.js New file .prettierrc.js Fix prettier issues identified in VSCode / "yarn lint".

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@next/next/no-img-element': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'no-console': ['error'],
+        '@next/next/no-img-element': ['warn'],
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import React from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function MyApp({ Component, pageProps }: AppProps) {
