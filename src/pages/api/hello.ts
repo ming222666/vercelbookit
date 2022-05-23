2a6504205cac562020ea40116534f6cdd0743c38
ming222555 <ming222555@yahoo.com>
2022-05-23 11:42:07 +0800

    This commit is to test out eslint both at VSCode and command line * test outcome: *** FAIL *** * FAIL errors - - Error: Definition for rule '@typescript-eslint/explicit-function-return-type' was not found. - Error: Definition for rule '@typescript-eslint/no-unused-vars' was not found. * changes made to ... ... .eslintrc.json ... package.json (change "next lint" to "next lint -d src") ... src/pages/index.tsx ... src/pages/api/hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
+function e() {
+  console.log(123);
+}
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
