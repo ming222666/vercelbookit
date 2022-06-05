baf619fd72f1250e50f03a3d4cfc998912c4c7f3
ming222555 <ming222555@yahoo.com>
2022-06-05 19:38:13 +0800

    Move content ( has only getUserProfile() function) from * to ** *  src/pages/api/user/profile.ts ** src/pages/api/auth/profile.ts Delete *

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,4 +1,4 @@
-import type { /* NextApiRequest */ NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -58,4 +58,32 @@ const registerUser = async (req: UserNextApiRequest, res: NextApiResponse<IUserD
   });
 };
 
-export { registerUser };
+// Get current user profile => /api/auth/profile
+const getUserProfile = async (req: NextApiRequest, res: NextApiResponse<IUserDto | IErrorDto>): Promise<void> => {
+  await db.connect();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const user: IUserDto = await User.findById((req as any).user._id).lean();
+  if (!user) {
+    await db.disconnect();
+    res.status(404).json({
+      errormsg: 'User not found with this ID',
+      status: 404,
+    });
+    return;
+  }
+  await db.disconnect();
+  res.status(200).send({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar,
+    role: user.role,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+    resetPasswordToken: user.resetPasswordToken,
+    resetPasswordExpire: user.resetPasswordExpire,
+  });
+};
+
+export { registerUser, getUserProfile };
--- src/pages/api/auth/profile.ts
+++ src/pages/api/auth/profile.ts
@@ -2,7 +2,7 @@ import nc from 'next-connect';
 
 import { onError, onNoMatch } from '../../../utils/onError';
 import { isAuth } from '../../../middlewares/auth';
-import { getUserProfile } from '../../../controllers/userControllers';
+import { getUserProfile } from '../../../controllers/authControllers';
 
 const handler = nc({ onError, onNoMatch });
 
