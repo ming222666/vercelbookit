3a4ee5830aaa6dc46cd034bd3282c7ce555769b8
ming222555 <ming222555@yahoo.com>
2022-06-07 12:19:21 +0800

    Redirect to home page if user already logined and try to goto Login / Register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
 
 import Layout from '../components/Layout';
 import Register from '../components/auth/Register';
@@ -10,6 +11,24 @@ const RegisterPage: NextPage = () => {
       <Register />
     </Layout>
   );
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default RegisterPage;
