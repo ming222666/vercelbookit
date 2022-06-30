68f297d888b8fd54f670b6ab5a043c2b80b918d3
ming222555 <ming222555@yahoo.com>
2022-06-30 14:03:36 +0800

    Some remarks at auth reducers to highlight purpose of user field in auth state

--- src/store/ducks/auth/reducers.ts
+++ src/store/ducks/auth/reducers.ts
@@ -2,11 +2,18 @@ import { AuthAction, AuthActionType } from './types';
 import { AuthState } from './models/AuthState';
 
 const initialState: AuthState = {
+  /**
+   * user here refers to logined user. It is here so Header can
+   * pick up stuff e.g. user name and to display, such as "Greeting! John".
+   * user is refreshed if logined user successfully updates
+   * his/her profile at My Profile page, so that Header will update
+   * greetings accordingly.
+   */
   user: null,
   error: null,
   /**
    * Set initialState.loading to true, so that
-   * ssr does not render [Login] button.
+   * ssr does not render [Login] button at Header.
    * O/w, there will be flickering between logined username (rendered client-side)
    * and [Login] button, when going to a different page that is ssr.
    */
